7bd111ce0394f800afeb5dfc8ec191e2700974d6
Ben Coveney <bencoveney@gmail.com>
2019-09-23 23:58:24 +0100

    perlin3 terrain

--- src/generateTerrain.ts
+++ src/generateTerrain.ts
@@ -1,27 +1,39 @@
 import * as noise from "./noise";
 
-export const CHUNK_SIZE = 100;
-const HEIGHT_SCALE = 10;
-const NOISE_SCALE = 100;
+export const CHUNK_SIZE = 50;
+export const CHUNK_HEIGHT = 10;
+const NOISE_SCALE = 10;
 
 export function generateTerrain(
   chunkX: number,
   chunkY: number,
   chunkZ: number
 ) {
-  const out = [];
+  const out: number[][] = [];
   for (let x = 0; x < CHUNK_SIZE; x++) {
-    for (let z = 0; z < CHUNK_SIZE; z++) {
-      let value = noise.perlin2(x / NOISE_SCALE, z / NOISE_SCALE);
-      let clamped = offset(value);
-      out.push(clamped);
+    let xAxis = [];
+    out.push(xAxis);
+    for (let y = 0; y < CHUNK_HEIGHT; y++) {
+      let yAxis = [];
+      xAxis.push(yAxis);
+      for (let z = 0; z < CHUNK_SIZE; z++) {
+        let value = noise.perlin3(
+          x / NOISE_SCALE,
+          y / NOISE_SCALE,
+          z / NOISE_SCALE
+        );
+        let clamped = offset(value);
+        yAxis.push(clamped);
+      }
     }
   }
   return out;
 }
 
 function offset(value) {
-  return Math.round(value * HEIGHT_SCALE) + HEIGHT_SCALE;
+  return value;
+  //const HEIGHT_SCALE = 10;
+  //return Math.round(value * HEIGHT_SCALE) + HEIGHT_SCALE;
 }
 
 export type Chunk = number[];
--- src/render.ts
+++ src/render.ts
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 
-import { generateTerrain, CHUNK_SIZE } from "./generateTerrain";
+import { generateTerrain, CHUNK_SIZE, CHUNK_HEIGHT } from "./generateTerrain";
 
 const terrain = generateTerrain(0, 0, 0);
 
-const SKY_COLOR = 0xdddddd;
+const SKY_COLOR = 0xffffff;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -27,27 +27,41 @@ document.body.appendChild(renderer.domElement);
 
 scene.add(new THREE.AmbientLight(SKY_COLOR));
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(directionalLight);
 
+const water = new THREE.Mesh(
+  new THREE.BoxGeometry(CHUNK_SIZE, 1, CHUNK_SIZE),
+  new THREE.MeshLambertMaterial({ color: 0x0000ff })
+);
+water.translateX(CHUNK_SIZE / 2);
+water.translateY(-1);
+water.translateZ(CHUNK_SIZE / 2);
+scene.add(water);
+
 const unitBox = new THREE.BoxGeometry(1, 1, 1);
-const green = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+const green = new THREE.MeshLambertMaterial({ color: 0x44aa33 });
 
 for (let x = 0; x < CHUNK_SIZE; x++) {
-  for (let z = 0; z < CHUNK_SIZE; z++) {
-    const cube = new THREE.Mesh(unitBox, green);
-    cube.translateX(x);
-    cube.translateZ(z);
+  for (let y = 0; y < CHUNK_HEIGHT; y++) {
+    for (let z = 0; z < CHUNK_SIZE; z++) {
+      const density = terrain[x][y][z] - y / CHUNK_HEIGHT;
 
-    const heightLookup = x * CHUNK_SIZE + z;
-    cube.translateY(terrain[heightLookup]);
+      if (density < 0) {
+        continue;
+      }
 
-    scene.add(cube);
+      const cube = new THREE.Mesh(unitBox, green);
+      cube.translateX(x + 0.5);
+      cube.translateY(y);
+      cube.translateZ(z + 0.5);
+      scene.add(cube);
+    }
   }
 }
 
 camera.position.x = CHUNK_SIZE;
-camera.position.y = CHUNK_SIZE / 3;
+camera.position.y = CHUNK_HEIGHT * 2;
 camera.position.z = CHUNK_SIZE;
 camera.lookAt(new THREE.Vector3(0, -CHUNK_SIZE / 2, 0));
 
